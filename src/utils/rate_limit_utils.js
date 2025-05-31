05130fafb52df8bff9054b5621e6129d3eb964a7
Dmitri Zaitsev <dmitri14@gmail.com>
2025-05-31 11:27:52 +0700

    refactor: update rate limit utilities and remove redundant RATE_LIMIT_TYPES declaration

--- src/utils/rate_limit_utils.js
+++ src/utils/rate_limit_utils.js
@@ -1,16 +1,20 @@
+const { RATE_LIMIT_TYPES } = require('../rate_limits');
+
 /**
  * Rate Limit Utilities
+ * 
+ * This module provides utilities for handling rate limits
+ * using constants imported from the rate_limits module.
  */
-const RATE_LIMIT_TYPES = {
-  RPM: 'requests_per_minute',
-  TPM: 'tokens_per_minute', 
-  RPD: 'requests_per_day',
-  TPD: 'tokens_per_day',
-  ASH: 'audio_seconds_per_hour',
-  ASD: 'audio_seconds_per_day',
-  IPM: 'images_per_minute',
-  VPM: 'videos_per_minute',
-  VPD: 'videos_per_day'
+function calculateBackoffDelay(attempt = 0, baseDelay = 1000, maxDelay = 30000) {
+  const exponential = baseDelay * Math.pow(2, attempt);
+  const jitter = Math.random() * 0.1 * exponential;
+  return Math.min(exponential + jitter, maxDelay);
+}
+
+module.exports = {
+  RATE_LIMIT_TYPES,
+  calculateBackoffDelay
 };
 
 /**
